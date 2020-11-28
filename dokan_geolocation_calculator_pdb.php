51199fc1f9aeee1a3e50c483438e738966843e3d
pooryaDb <poorya.db@gmail.com>
2020-11-28 22:13:51 +0330

    define 'DGCPDB_PLUGIN_NAME'

--- dokan_geolocation_calculator_pdb.php
+++ dokan_geolocation_calculator_pdb.php
@@ -36,6 +36,7 @@ if (!defined('WPINC')) {
  * Rename this for your plugin and update it as you release new versions.
  */
 define('DGCPDB_VERSION', '1.0.0');
+define('DGCPDB_PLUGIN_NAME', __('Dokan Geolocation Calculator', 'dgcpdb'));
 
 /**
  * The code that runs during plugin activation.
